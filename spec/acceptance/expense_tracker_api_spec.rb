70b7a9afaa8f5f37dab10803c717ab6df8a96dd2
Ruto Collins <borecollins@gmail.com>
2018-06-25 09:18:44 +0300

    Acceptance tests: It records submitted data

---
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rack/test'
+require 'json'
+
+require_relative '../../app/api'
+
+module ExpenseTracker
+  RSpec.describe 'Expense Tracker API' do
+    include Rack::Test::Methods
+
+    def app
+      ExpenseTracker::API.new
+    end
+
+    it 'records submitted expenses' do
+      coffee = {
+        'payee' => 'Starbucks',
+        'amount' => 5.75,
+        'date' => '2018/06/25'
+      }
+
+      post '/expenses', JSON.generate(coffee)
+      expect(last_response.status).to eq(200)
+
+      parsed = JSON.parse(last_response.body)
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+    end
+  end
+end
